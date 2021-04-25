5a90cfb700bffd45b75e9b9c5fa8d8cb9966ef05
fooofi <sofiabarrientos.r@gmail.com>
2021-04-24 23:21:51 -0400

    add data component

---
+++ src/Data.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+
+function Data(){
+  const url = 'https://www.telemercados.cl/api/catalog_system/pub/products/search?fq=C:349&_from=1&_to=49'
+  const [data, setData] = useState()
+  
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setData(responseJSON)
+  }
+  useEffect(() =>{
+    fetchApi()
+  }, [])
+
+  const group = (items, key) => items.reduce(
+    (result, item) => ({
+      ...result,
+      [item[key]]: [
+        ...(result[item[key]] || []),
+        [item.productName, item.brand, item.link]
+      ],
+    }), 
+    {},
+  );
+
+if(data){
+  const brand = group(data, 'brand')
+  console.log(brand)
+}
+
+  return(
+    <div>
+      <h1>HolaMundo</h1>
+      <p>En la consola se encuentra la data</p>
+    </div>
+  )
+}
+
+export default Data;
